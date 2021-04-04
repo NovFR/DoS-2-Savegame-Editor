1e4b35a36a11f4fe73beb51308ca59bfc8da3026
NovFR <50532103+NovFR@users.noreply.github.com>
2021-04-04 18:16:41 +0200

    Changed about msg

--- Source/Sources/Main/Main_About.c
+++ Source/Sources/Main/Main_About.c
@@ -16,6 +16,7 @@
 #include "About.h"
 #include "Locale.h"
 #include "Dialogs.h"
+#include "Utils.h"
 
 extern APPLICATION		App;
 
@@ -30,12 +31,44 @@ void About_Display(HWND hWnd)
 {
 	TASKDIALOGCONFIG	dialog;
 	WCHAR*			pszAbout;
-	DWORD_PTR		vl[4];
+	WCHAR*			pszText;
+	WCHAR			szYear[] = L"0000";
+	WCHAR			szMonth[] = L"00";
+	WCHAR			szDay[] = L"00";
+	WCHAR			szHour[] = L"00";
+	WCHAR			szMinutes[] = L"00";
+	DWORD_PTR		vl[6];
+	int			i;
 
-	vl[0] = VERSION_MAJOR;
-	vl[1] = REVISION_MAJOR;
-	vl[2] = VERSION_MINOR;
-	vl[3] = REVISION_MINOR;
+	static WCHAR*		pszMonths[] = { L"Jan", L"Feb", L"Mar", L"Apr", L"May", L"Jun", L"Jul", L"Aug", L"Sep", L"Oct", L"Nov", L"Dec", NULL };
+
+	pszText = TOWIDECHAR(VERSION_TIME);
+	szHour[0] = pszText[0];
+	szHour[1] = pszText[1];
+	szMinutes[0] = pszText[3];
+	szMinutes[1] = pszText[4];
+
+	pszText = TOWIDECHAR(VERSION_DATE);
+	szYear[0] = pszText[7];
+	szYear[1] = pszText[8];
+	szYear[2] = pszText[9];
+	szYear[3] = pszText[10];
+	szDay[0] = pszText[4] > L'0'?pszText[4]:L'0';
+	szDay[1] = pszText[5];
+
+	for (i = 0; pszMonths[i] != NULL; i++)
+		{
+		if (wcsncmp(pszMonths[i],pszText,3)) continue;
+		Misc_Printf(szMonth,3,L"%02u",i+1);
+		break;
+		}
+
+	vl[0] = (DWORD_PTR)TOWIDECHAR(VERSION_STRING_NZ);
+	vl[1] = (DWORD_PTR)szDay;
+	vl[2] = (DWORD_PTR)szMonth;
+	vl[3] = (DWORD_PTR)szYear;
+	vl[4] = (DWORD_PTR)szHour;
+	vl[5] = (DWORD_PTR)szMinutes;
 
 	if (FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER|FORMAT_MESSAGE_FROM_STRING|FORMAT_MESSAGE_ARGUMENT_ARRAY,Locale_GetText(TEXT_ABOUT),0,0,(WCHAR *)&pszAbout,1,(va_list *)&vl))
 		{
